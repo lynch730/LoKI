819bf7ffa5aff519ae9853ca456383c6b31738bd
Joel Lynch <lynch@iastate.edu>
2023-07-18 07:56:19 -0500

    fix to rate coefficients

--- Code/run_pulse.m
+++ Code/run_pulse.m
@@ -1,7 +1,11 @@
 
 function run_pulse(N)
 
-	setup = Setup(['pulseN',sprintf('%i', N),'.in']);
+    if ~exist('N', 'var')
+        N = 600;
+    end
+
+	setup = Setup(['pulse_N',sprintf('%i', N),'.in']);
 	electronKinetics = setup.initializeSimulation();
 	electronKinetics.solve();
 	setup.finishSimulation();
